ea0c518a810cacf3ce35a40352e5bf71fcac3d89
Matt Wagner <m@ttwagner.com>
2015-03-01 21:36:36 -0500

    Cheap hack to rewrite static assets to use CloudFront

---
+++ maw_cdn_rewriter/index.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @package maw_cdn_rewriter
+ * @version 0.0
+ */
+/*
+Plugin Name: MAW CDN Rewriter
+Plugin URI: http://blogs.n1zyy.com/
+Description: Tailored for blogs.n1zyy.com; rewrite static content to use a CDN for faster loading.
+Author: Matt Wagner
+Version: 0.0
+Author URI: http://ma.ttwagner.com/
+*/
+
+/*
+function sub_cdn($content='') {
+  $source = '/http:\/\/blogs.n1zyy.com\/(\w+)\/wp-content/';
+  $rep = 'http:\/\/blogscdn.n1zyy.com\/$1\/wp-content';
+  return preg_replace($source, $rep, $content);
+}
+
+add_filter('the_content', 'sub_cdn');
+*/
+
+function sub_cdn($content='') {
+    ob_start(function($content){
+      $source = '/http:\/\/blogs.n1zyy.com\/(\w+)\/wp-content/';
+      $rep = '//d1uwqsybio33ca.cloudfront.net/$1/wp-content';
+      $f = preg_replace($source, $rep, $content);
+
+      $source = '/http:\/\/blogs.n1zyy.com/';
+      $rep = '';
+      return preg_replace($source, $rep, $f);
+    });
+
+    //return preg_replace($source, $rep, $content);
+}
+
+add_action('template_redirect', 'sub_cdn');
+
+?>
